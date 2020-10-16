07c1fdf0075b62797b91c3a4c9f628378ad86491
Federico Aloi <federico.aloi@gmail.com>
2020-10-16 11:30:08 -0300

    Renombrado proyecto

---
+++ src/main/kotlin/ar/edu/unahur/obj2/minions/Minion.kt
@@ -0,0 +1,4 @@
+package ar.edu.unahur.obj2.minions
+
+class Minion {
+}
---
+++ src/test/kotlin/ar/edu/unahur/obj2/minions/MinionTest.kt
@@ -0,0 +1,12 @@
+package ar.edu.unahur.obj2.minions
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.lang.ArithmeticException
+
+class MinionTest : DescribeSpec({
+  describe("Un minion") {
+    // Zaaaaaaaaaarán
+  }
+})
